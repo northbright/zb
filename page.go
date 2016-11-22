0f0ac1430b3f7b175636b8c81e1fb0868585eeff
Frank Xu <frank.xu@northbright.com>
2016-11-22 11:33:48 +0800

    Remove example school name in title.

--- page.go
+++ page.go
@@ -9,12 +9,12 @@ import (
 
 func getZB(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"title": "长宁美校转班申请",
+		"title": "转班申请",
 	})
 }
 
 func postZB(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"title": "长宁美校转班申请",
+		"title": "转班申请",
 	})
 }
