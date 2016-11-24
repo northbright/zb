28dfcb11baa839965c9f85aa224498307480240b
Frank Xu <frank.xu@northbright.com>
2016-11-25 00:26:00 +0800

    Add validate.go to validate post form.

--- api.go
+++ api.go
@@ -160,8 +160,14 @@ end:
 }
 
 func postZB(c *gin.Context) {
+	var err error
+	var conn redis.Conn
+	var n, periodNum int64
+	var k0, k1, k2 = "", "", ""
+	campuses := []string{}
 	err_msg := ""
-	//k := ""
+	k := ""
+	valid := false
 	record := ""
 	name := c.DefaultPostForm("name", "")
 	tel := c.DefaultPostForm("tel", "")
@@ -186,6 +192,81 @@ func postZB(c *gin.Context) {
 	if name == "" || tel == "" || grade == "" || currentCampus == "" || currentPeriod == "" || wantedCampus1 == "" || wantedPeriod1 == "" {
 		err_msg = "信息不完整，请返回重新填写."
 		goto end
+	}
+
+	if valid = validateName(name); !valid {
+		err_msg = "学生姓名有误，请重新填写."
+		goto end
+	}
+
+	if valid = validateTel(tel); !valid {
+		err_msg = "联系手机号码有误，请重新填写."
+		goto end
+	}
+
+	if conn, err = GetRedisConn(redisAddr, redisPassword); err != nil {
+		err_msg = "连接数据库失败."
+		goto end
+	}
+	defer conn.Close()
+
+	if valid = validatePeriod(conn, grade, currentCampus, currentPeriod); !valid {
+		err_msg = "当前时段有误：校区，年级与时段不匹配."
+		goto end
+	}
+
+	if valid = validatePeriod(conn, grade, wantedCampus1, wantedPeriod1); !valid {
+		err_msg = "期望时段（首选）有误：校区，年级与时段不匹配."
+		goto end
+	}
+
+	if valid = validatePeriod(conn, grade, wantedCampus2, wantedPeriod2); !valid {
+		err_msg = "期望时段（备选）有误：校区，年级与时段不匹配."
+		goto end
+	}
+
+	k = fmt.Sprintf("%v:campuses", grade)
+	if campuses, err = redis.Strings(conn.Do("SMEMBERS", k)); err != nil {
+		err_msg = "获取年级对应校区信息失败."
+		goto end
+	}
+
+	for _, campus := range campuses {
+		k = fmt.Sprintf("%v/%v", campus, grade)
+		if n, err = redis.Int64(conn.Do("ZCARD", k)); err != nil {
+			err_msg = "获取年级校区对应时段数量失败."
+			goto end
+		}
+		log.Printf("k: %v, n: %v\n", k, n)
+		periodNum += n
+	}
+
+	k0 = fmt.Sprintf("%v/%v", currentCampus, currentPeriod)
+	k1 = fmt.Sprintf("%v/%v", wantedCampus1, wantedPeriod1)
+	k2 = fmt.Sprintf("%v/%v", wantedCampus2, wantedPeriod2)
+
+	if periodNum <= 1 {
+		err_msg = "无可选时间段，不能转班."
+		goto end
+	} else if periodNum == 2 {
+		if k0 == k1 {
+			err_msg = "当前时段与期望时段（首选）一致，请重新选择."
+			goto end
+		}
+
+	} else if periodNum >= 3 {
+		if k0 == k1 {
+			err_msg = "当前时段与期望时段（首选）一致，请重新选择."
+			goto end
+		}
+		if k0 == k2 {
+			err_msg = "当前时段与期望时段（备选）一致，请重新选择."
+			goto end
+		}
+		if k1 == k2 {
+			err_msg = "期望时段（首选）与（备选）一致，请重新选择."
+			goto end
+		}
 	}
 
 	record = fmt.Sprintf("%v,%v,%v,%v,%v,%v,%v,%v,%v", name, tel, grade, currentCampus, currentPeriod, wantedCampus1, wantedPeriod1, wantedCampus2, wantedPeriod2)
---
+++ validate.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+func validateName(name string) bool {
+	if len(name) < 6 || len(name) > 60 {
+		return false
+	}
+	return true
+}
+
+func validateTel(tel string) bool {
+	p := `^\d{11}$`
+	r := regexp.MustCompile(p)
+
+	if r.MatchString(tel) {
+		return true
+	}
+	return false
+}
+
+func validatePeriod(c redis.Conn, grade, campus, period string) bool {
+	valid := false
+	var err error
+	exists := false
+	k := ""
+	score := ""
+
+	k = fmt.Sprintf("%v/%v", campus, grade)
+	if exists, err = redis.Bool(c.Do("EXISTS", k)); err != nil {
+		goto end
+	}
+
+	if !exists {
+		goto end
+	}
+
+	if score, err = redis.String(c.Do("ZSCORE", k, period)); err != nil {
+		goto end
+	}
+
+	if score == "" {
+		goto end
+	}
+
+	valid = true
+end:
+	return valid
+}
